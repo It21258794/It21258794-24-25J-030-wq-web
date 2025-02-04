114c74357da0204fca06ba354ed0a3fe562d2143
Shamry Shiraz <IT21277054@my.sliit.lk>
2025-02-04 22:11:04 +0530

    feat(waterqualityprediction): implemented water quality prediction
    Signed-off-by: Shamry Shiraz <IT21277054@my.sliit.lk>

---
+++ wq-web/src/pages/Water Quality Prediction/WaterQualityPrediction.tsx
@@ -0,0 +1,264 @@
+import {
+  Box,
+  Card,
+  Chip,
+  FormControl,
+  Grid2,
+  InputLabel,
+  MenuItem,
+  Select,
+  SelectChangeEvent,
+  Table,
+  TableBody,
+  TableCell,
+  TableFooter,
+  TableHead,
+  TablePagination,
+  TableRow,
+  Typography,
+} from "@mui/material";
+import TablePaginationActions from "@mui/material/TablePagination/TablePaginationActions";
+import { LineChart } from "@mui/x-charts";
+import { useState } from "react";
+import "./WaterQualityPrediction.css";
+import React from "react";
+
+const WaterQualityPrediction = (): JSX.Element => {
+  const [page, setPage] = useState(0);
+  const [rowsPerPage, setRowsPerPage] = useState(5);
+  const [Turbidity, setTurbidity] = React.useState("");
+  const [Week, setWeek] = React.useState("");
+
+  const handleChange = (event: SelectChangeEvent) => {
+    if (event.target.name === "week") {
+      setWeek(event.target.value);
+    } else if (event.target.name === "turbidity") {
+      setTurbidity(event.target.value);
+    }
+  };
+
+  const rows = [
+    {
+      date: "2024-02-04",
+      parameter: "Turbidity",
+      predictedValue: 5.4,
+      rainfall: 12,
+      flowRate: 3.2,
+      depth: 1.5,
+      threshold: 7,
+      status: "Active",
+    },
+    {
+      date: "2024-02-05",
+      parameter: "pH",
+      predictedValue: 7.2,
+      rainfall: 8,
+      flowRate: 2.9,
+      depth: 1.3,
+      threshold: 6.5,
+      status: "Inactive",
+    },
+  ];
+
+  return (
+    <>
+      <Grid2
+        container
+        spacing={2}
+        justifyContent="center"
+        sx={{ marginTop: "20px" }}
+      ></Grid2>
+      <Grid2
+        container
+        spacing={10}
+        justifyContent="center"
+        sx={{ marginTop: "20px", display: "flex", justifyContent: "center" }}
+      >
+        <Card className="lineChartCard">
+          <Box
+            display="flex"
+            alignItems="center"
+            justifyContent="space-between"
+            width="100%"
+          >
+            <Typography className="typographyStyles">
+              Turbidity Prediction
+            </Typography>
+            <Box display="flex" gap={2}>
+              <FormControl sx={{ m: 1, minWidth: 130 }} size="small">
+                <InputLabel id="demo-select-small-label">Turbidity</InputLabel>
+                <Select
+                  name="turbidity"
+                  labelId="demo-select-small-label"
+                  id="demo-select-small"
+                  value={Turbidity}
+                  label="Turbidity"
+                  onChange={handleChange}
+                >
+                  <MenuItem value="">
+                    <em>None</em>
+                  </MenuItem>
+                  <MenuItem value={10}>Ten</MenuItem>
+                  <MenuItem value={20}>Twenty</MenuItem>
+                  <MenuItem value={30}>Thirty</MenuItem>
+                </Select>
+              </FormControl>
+              <FormControl sx={{ m: 1, minWidth: 130 }} size="small">
+                <InputLabel id="demo-select-small-label">Next Week</InputLabel>
+                <Select
+                  name="week"
+                  labelId="demo-select-small-label"
+                  id="demo-select-small"
+                  value={Week}
+                  label="Last Week"
+                  onChange={handleChange}
+                >
+                  <MenuItem value="">
+                    <em>None</em>
+                  </MenuItem>
+                  <MenuItem value={10}>Ten</MenuItem>
+                  <MenuItem value={20}>Twenty</MenuItem>
+                  <MenuItem value={30}>Thirty</MenuItem>
+                </Select>
+              </FormControl>
+            </Box>
+          </Box>
+          <></>
+          <LineChart
+            xAxis={[{ data: [1, 2, 3, 5, 8, 10] }]}
+            series={[
+              {
+                data: [2, 5.5, 2, 8.5, 1.5, 5],
+              },
+            ]}
+            width={720}
+            height={300}
+          />
+        </Card>
+        <Card className="lineChartCard">
+          <Box
+            display="flex"
+            alignItems="center"
+            justifyContent="space-between"
+            width="100%"
+          >
+            <Typography className="typographyStyles">
+              Turbidity Analysis
+            </Typography>
+            <Box display="flex" gap={2}>
+              <FormControl sx={{ m: 1, minWidth: 130 }} size="small">
+                <InputLabel id="demo-select-small-label">Turbidity</InputLabel>
+                <Select
+                  name="turbidity"
+                  labelId="demo-select-small-label"
+                  id="demo-select-small"
+                  value={Turbidity}
+                  label="Turbidity"
+                  onChange={handleChange}
+                >
+                  <MenuItem value="">
+                    <em>None</em>
+                  </MenuItem>
+                  <MenuItem value={10}>Ten</MenuItem>
+                  <MenuItem value={20}>Twenty</MenuItem>
+                  <MenuItem value={30}>Thirty</MenuItem>
+                </Select>
+              </FormControl>
+              <FormControl sx={{ m: 1, minWidth: 130 }} size="small">
+                <InputLabel id="demo-select-small-label">Last Week</InputLabel>
+                <Select
+                  name="week"
+                  labelId="demo-select-small-label"
+                  id="demo-select-small"
+                  value={Week}
+                  label="Last Week"
+                  onChange={handleChange}
+                >
+                  <MenuItem value="">
+                    <em>None</em>
+                  </MenuItem>
+                  <MenuItem value={10}>Ten</MenuItem>
+                  <MenuItem value={20}>Twenty</MenuItem>
+                  <MenuItem value={30}>Thirty</MenuItem>
+                </Select>
+              </FormControl>
+            </Box>
+          </Box>
+          <></>
+          <LineChart
+            xAxis={[{ data: [1, 2, 3, 5, 8, 10] }]}
+            series={[
+              { color: "green", data: [0, 5, 2, 6, 3, 9.3] },
+              { color: "red", data: [6, 3, 7, 9.5, 4, 2] },
+              { color: "blue", data: [3, 1, 3, 6, 2, 1] },
+            ]}
+            width={720}
+            height={300}
+          />
+        </Card>
+        <Card className="cardContainer">
+          <Table className="table">
+            <TableHead>
+              <TableRow>
+                {[
+                  "Date",
+                  "Parameter",
+                  "Predicted Value",
+                  "Rainfall (mm)",
+                  "Flow Rate (m³/s)",
+                  "Depth (m)",
+                  "Threshold Value",
+                  "Status",
+                ].map((header) => (
+                  <TableCell key={header} className="table-header">
+                    {header}
+                  </TableCell>
+                ))}
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {rows.map((row, index) => (
+                <TableRow key={index}>
+                  <TableCell className="table-cell">{row.date}</TableCell>
+                  <TableCell className="table-cell">{row.parameter}</TableCell>
+                  <TableCell className="table-cell">
+                    {row.predictedValue}
+                  </TableCell>
+                  <TableCell className="table-cell">{row.rainfall}</TableCell>
+                  <TableCell className="table-cell">{row.flowRate}</TableCell>
+                  <TableCell className="table-cell">{row.depth}</TableCell>
+                  <TableCell className="table-cell">{row.threshold}</TableCell>
+                  <TableCell className="table-cell">
+                    <Chip
+                      label={row.status}
+                      className={`status-badge ${
+                        row.status === "Active"
+                          ? "status-active"
+                          : "status-inactive"
+                      }`}
+                    />
+                  </TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+            <TableFooter>
+              <TableRow>
+                <TablePagination
+                  className="pagination"
+                  rowsPerPageOptions={[5, 10, 25, { label: "All", value: -1 }]}
+                  colSpan={5}
+                  count={rows.length}
+                  rowsPerPage={rowsPerPage}
+                  page={page}
+                  ActionsComponent={TablePaginationActions}
+                />
+              </TableRow>
+            </TableFooter>
+          </Table>
+        </Card>
+      </Grid2>
+    </>
+  );
+};
+
+export default WaterQualityPrediction;
