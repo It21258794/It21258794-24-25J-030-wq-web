eaf2f15410a0479827ac31f8e4556cff15e70b7d
It21258794 <it21258794@my.sliit.lk>
2025-02-05 00:39:52 +0530

    turbidity data dynamicaly added

--- wq-web/src/pages/Water Quality Prediction/WaterQualityPrediction.tsx
+++ wq-web/src/pages/Water Quality Prediction/WaterQualityPrediction.tsx
@@ -22,6 +22,45 @@ import React from "react";
 const WaterQualityPrediction = (): JSX.Element => {
   const [Turbidity, setTurbidity] = React.useState("");
   const [Week, setWeek] = React.useState("");
+  const [predictions, setPredictions] = React.useState([]);
+  const [dates, setDates] = React.useState([]);
+
+  React.useEffect(() => {
+    const fetchPredictions = async () => {
+      const requestData = {
+        last_known_data: [
+          3.5, 3.6, 3.7, 3.8, 3.6, 3.5, 3.4, 3.3, 3.2, 3.1,
+          3.0, 2.9, 2.8, 2.7, 2.6, 2.5, 2.4, 2.3, 2.2, 2.1,
+          2.0, 1.9, 1.8, 1.7, 1.6, 1.5, 1.4, 1.3, 1.2, 1.1,
+        ], 
+        days: 7,
+      };
+
+      try {
+        const response = await fetch("http://127.0.0.1:5000/predict", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(requestData),
+        });
+
+        const data = await response.json();
+
+        if (response.ok) {
+          console.log(data.dates);
+          setDates(data.dates);
+          setPredictions(data.predictions);
+        } else {
+          throw new Error(data.error || "Failed to fetch predictions");
+        }
+      } catch (err:any) {
+        console.log(err.message);
+      }
+    };
+
+    fetchPredictions();
+  }, []);
 
   const handleChange = (event: SelectChangeEvent) => {
     if (event.target.name === "week") {
@@ -119,15 +158,11 @@ const WaterQualityPrediction = (): JSX.Element => {
           </Box>
           <></>
           <LineChart
-            xAxis={[{ data: [1, 2, 3, 5, 8, 10] }]}
-            series={[
-              {
-                data: [2, 5.5, 2, 8.5, 1.5, 5],
-              },
-            ]}
-            width={720}
-            height={300}
-          />
+  xAxis={[{ scaleType: "point", data: dates }]} // Ensure scaleType is set correctly
+  series={[{ data: predictions }]} // Ensure predictions align with dates
+  width={720}
+  height={300}
+/>
         </Card>
         <Card className="lineChartCard">
           <Box
