604ede345a7cc90323c0d05524f1a3440c1cdb30
It21258794 <it21258794@my.sliit.lk>
2025-03-18 19:40:31 +0530

    fix(water-quality-module):line chart color changed

--- wq-web/src/pages/Water Quality Prediction/WaterQualityPrediction.tsx
+++ wq-web/src/pages/Water Quality Prediction/WaterQualityPrediction.tsx
@@ -43,9 +43,21 @@ const WaterQualityPrediction = (): JSX.Element => {
   const [totalPages, setTotalPages] = React.useState(1);
   const [realData, setRealData] = React.useState([]);
 
-const turbidity =[9.09, 9.09, 6.8, 6.6, 7.9, 8.1, 20.2, 157, 2.5, 3.2, 3.5, 3.5, 2.8, 3.2, 5, 5.2, 3.7, 6.6, 19.8, 2.5, 3.1, 2.1, 2.3, 3.2, 7.1, 2.7, 2.5, 1.3, 1.3, 3.5, 2.1, 3.3, 2.91, 3.8, 3.9, 7.81]
-const ph = [6.8, 6.8, 6.8, 6.8, 6.8, 6.8, 6.7, 6.8, 6.7, 6.7, 6.8, 6.7, 6.8, 6.7, 6.8, 6.6, 6.7, 6.7, 6.6, 6.6, 6.7, 6.7, 6.8, 6.7, 6.8, 6.9, 6.8, 6.7, 6.8, 6.7, 6.6, 6.7, 6.7, 6.6, 6.7, 6.7]
-const conductivity  =[58.6, 58.6, 58.3, 56.1, 54.3, 54.3, 54.3, 54.3, 56.1, 54.3, 54.3, 54.3, 54.9, 57.7, 56.2, 56, 54.2, 55.1, 57.8, 54.7, 59.1, 59, 55.1, 55, 55.7, 53, 52.9, 57.6, 56.2, 55.5, 54.6, 58.9, 54.2, 55.1, 50.1, 51.2, 57.4]
+  const turbidity = [
+    9.09, 9.09, 6.8, 6.6, 7.9, 8.1, 20.2, 157, 2.5, 3.2, 3.5, 3.5, 2.8, 3.2, 5,
+    5.2, 3.7, 6.6, 19.8, 2.5, 3.1, 2.1, 2.3, 3.2, 7.1, 2.7, 2.5, 1.3, 1.3, 3.5,
+    2.1, 3.3, 2.91, 3.8, 3.9, 7.81
+  ];
+  const ph = [
+    6.8, 6.8, 6.8, 6.8, 6.8, 6.8, 6.7, 6.8, 6.7, 6.7, 6.8, 6.7, 6.8, 6.7, 6.8,
+    6.6, 6.7, 6.7, 6.6, 6.6, 6.7, 6.7, 6.8, 6.7, 6.8, 6.9, 6.8, 6.7, 6.8, 6.7,
+    6.6, 6.7, 6.7, 6.6, 6.7, 6.7
+  ];
+  const conductivity = [
+    58.6, 58.6, 58.3, 56.1, 54.3, 54.3, 54.3, 54.3, 56.1, 54.3, 54.3, 54.3,
+    54.9, 57.7, 56.2, 56, 54.2, 55.1, 57.8, 54.7, 59.1, 59, 55.1, 55, 55.7, 53,
+    52.9, 57.6, 56.2, 55.5, 54.6, 58.9, 54.2, 55.1, 50.1, 51.2, 57.4
+  ];
 
   const token: any = authContext?.token;
 
@@ -69,15 +81,14 @@ const conductivity  =[58.6, 58.6, 58.3, 56.1, 54.3, 54.3, 54.3, 54.3, 56.1, 54.3
     authContext?.setIsLoading(true);
     setError(null);
     try {
-      
       setTimeout(async () => {
         const result = await getPredictions(days, parameter, isPast, token);
-      const formattedDates = result.dates.map((dateString: string) => {
-        const date = new Date(dateString);
-        return date.toISOString().split("T")[0];
-      });
-      setPredictions(result.values);
-      setDates(formattedDates);
+        const formattedDates = result.dates.map((dateString: string) => {
+          const date = new Date(dateString);
+          return date.toISOString().split('T')[0];
+        });
+        setPredictions(result.values);
+        setDates(formattedDates);
       }, 1000);
       authContext?.setIsLoading(false);
     } catch (err) {
@@ -100,12 +111,12 @@ const conductivity  =[58.6, 58.6, 58.3, 56.1, 54.3, 54.3, 54.3, 54.3, 56.1, 54.3
       const result = await getPredictions(days, parameter, isPast, token);
       const formattedDates = result.dates.map((dateString: string) => {
         const date = new Date(dateString);
-        return date.toISOString().split("T")[0];
+        return date.toISOString().split('T')[0];
       });
       setPastPredictions(result.values);
       setPastDates(formattedDates);
-      
-      let filteredRealData:any = [];
+
+      let filteredRealData: any = [];
       if (pastParameter === 'turbidity') {
         filteredRealData = turbidity.slice(0, result.values.length);
       } else if (pastParameter === 'ph') {
@@ -139,7 +150,7 @@ const conductivity  =[58.6, 58.6, 58.3, 56.1, 54.3, 54.3, 54.3, 54.3, 56.1, 54.3
         token
       );
       console.log(result);
-      
+
       setTimeout(async () => {
         authContext?.setIsLoading(false);
         if (result && Array.isArray(result.content)) {
@@ -150,7 +161,6 @@ const conductivity  =[58.6, 58.6, 58.3, 56.1, 54.3, 54.3, 54.3, 54.3, 56.1, 54.3
           setError('No valid data found');
         }
       }, 1000);
-      
     } catch (err) {
       setError('Failed to fetch predictions');
     } finally {
@@ -210,142 +220,225 @@ const conductivity  =[58.6, 58.6, 58.3, 56.1, 54.3, 54.3, 54.3, 54.3, 56.1, 54.3
         sx={{marginTop: '20px', display: 'flex', justifyContent: 'center'}}
       >
         <Card className="lineChartCard">
-          <Box
-            display="flex"
-            alignItems="center"
-            justifyContent="space-between"
-            width="100%"
-          >
-            <Typography className="typographyStyles">
-              {parameter.charAt(0).toUpperCase() + parameter.slice(1)}{' '}
-              Prediction
+          <Box width="100%">
+            <Typography
+              className="typographyStyles"
+              sx={{
+                textAlign: 'center',
+                mb: 1,
+                textTransform: 'uppercase',
+                color: '#102D4D',
+                fontWeight: 'bold',
+                // marginBottom:"30px"
+              }}
+            >
+              {parameter.toUpperCase()} PREDICTIONS FOR{' '}
+              {predictionPeriod === 7
+                ? 'NEXT WEEK'
+                : predictionPeriod === 14
+                ? 'NEXT TWO WEEKS'
+                : 'NEXT MONTH'}
             </Typography>
-            <Box display="flex" gap={1}>
-              <FormControl sx={{m: 1, minWidth: 60}} size="small">
-                <Select
-                  name="parameter"
-                  id="demo-select-small"
-                  value={parameter}
-                  onChange={handleParameterChange}
-                  sx={{
-                    width: '110px',
-                    height: '30px',
-                    fontSize: '12px',
-                    borderRadius: 3,
-                    '& .MuiSelect-select': {
-                      padding: '10px'
-                    }
-                  }}
-                >
-                  <MenuItem value={'ph'}>Ph</MenuItem>
-                  <MenuItem value={'turbidity'}>Turbidity</MenuItem>
-                  <MenuItem value={'conductivity'}>Conductivity</MenuItem>
-                </Select>
-              </FormControl>
-              <FormControl sx={{m: 1, minWidth: 130}} size="small">
-                <Select
-                  name="predictionPeriod"
-                  id="demo-select-small"
-                  value={String(predictionPeriod)}
-                  onChange={handlePeriodChange}
-                  sx={{
-                    width: '110px',
-                    height: '30px',
-                    fontSize: '12px',
-                    borderRadius: 3,
-                    '& .MuiSelect-select': {
-                      padding: '10px'
-                    }
-                  }}
-                >
-                  <MenuItem value={7}>Next Week</MenuItem>
-                  <MenuItem value={14}>Next Two Weeks</MenuItem>
-                  <MenuItem value={30}>Next Month</MenuItem>
-                </Select>
-              </FormControl>
+            <Box
+              display="flex"
+              alignItems="center"
+              justifyContent="flex-end"
+              width="100%"
+            >
+              <Box display="flex" gap={1}>
+                <FormControl sx={{m: 1, minWidth: 60}} size="small">
+                  <Select
+                    name="parameter"
+                    id="demo-select-small"
+                    value={parameter}
+                    onChange={handleParameterChange}
+                    sx={{
+                      width: '110px',
+                      height: '30px',
+                      fontSize: '12px',
+                      borderRadius: 3,
+                      '& .MuiSelect-select': {
+                        padding: '10px'
+                      }
+                    }}
+                  >
+                    <MenuItem value={'ph'}>Ph</MenuItem>
+                    <MenuItem value={'turbidity'}>Turbidity</MenuItem>
+                    <MenuItem value={'conductivity'}>Conductivity</MenuItem>
+                  </Select>
+                </FormControl>
+
+                <FormControl sx={{m: 1, minWidth: 130}} size="small">
+                  <Select
+                    name="predictionPeriod"
+                    id="demo-select-small"
+                    value={String(predictionPeriod)}
+                    onChange={handlePeriodChange}
+                    sx={{
+                      width: '110px',
+                      height: '30px',
+                      fontSize: '12px',
+                      borderRadius: 3,
+                      '& .MuiSelect-select': {
+                        padding: '10px'
+                      }
+                    }}
+                  >
+                    <MenuItem value={7}>Next Week</MenuItem>
+                    <MenuItem value={14}>Next Two Weeks</MenuItem>
+                    <MenuItem value={30}>Next Month</MenuItem>
+                  </Select>
+                </FormControl>
+              </Box>
             </Box>
           </Box>
-          <Box >
-          <LineChart
-            xAxis={[{ scaleType: 'point', data: dates }]}
-            series={[{ data: predictions }]}
-            height={300}
-            margin={{ left: 70 }}
-          />
-        </Box>
+
+          <Box>
+            <LineChart
+              xAxis={[{scaleType: 'point', data: dates}]}
+              series={[{data: predictions}]}
+              height={300}
+              margin={{left: 70}}
+            />
+          </Box>
+          <Box display="flex" justifyContent="center" gap={2} mt={1}>
+            <Box display="flex" alignItems="center">
+              <Box
+                sx={{
+                  width: 12,
+                  height: 12,
+                  backgroundColor: '#5F99AE',
+                  borderRadius: '50%',
+                  mr: 1
+                }}
+              />
+              <Typography variant="body2">Predicted Data</Typography>
+            </Box>
+            
+          </Box>
         </Card>
         <Card className="lineChartCard">
-      <Box display="flex" alignItems="center" justifyContent="space-between" width="100%">
-        <Typography className="typographyStyles">
-          {pastParameter.charAt(0).toUpperCase() + pastParameter.slice(1)} Analysis
-        </Typography>
-        <Box display="flex" gap={1}>
-          <FormControl sx={{ m: 1, minWidth: 60 }} size="small">
-            <Select
-              name="turbidity"
-              id="demo-select-small"
-              value={String(pastParameter)}
-              onChange={handlePastParameterChange}
+          <Box width="100%">
+            <Typography
+              className="typographyStyles"
               sx={{
-                width: '110px',
-                height: '30px',
-                fontSize: '12px',
-                borderRadius: 3,
-                '& .MuiSelect-select': {
-                  padding: '10px',
-                },
+                textAlign: 'center',
+                mb: 1,
+                textTransform: 'uppercase',
+                color: '#102D4D',
+                fontWeight: 'bold'
               }}
             >
-              <MenuItem value={'ph'}>Ph</MenuItem>
-              <MenuItem value={'turbidity'}>Turbidity</MenuItem>
-              <MenuItem value={'conductivity'}>Conductivity</MenuItem>
-            </Select>
-          </FormControl>
-          <FormControl sx={{ m: 1, minWidth: 130 }} size="small">
-            <Select
-              name="week"
-              id="demo-select-small"
-              value={String(pastPredictionPeriod)}
-              onChange={handlePastPeriodChange}
-              sx={{
-                width: '110px',
-                height: '30px',
-                fontSize: '12px',
-                borderRadius: 3,
-                '& .MuiSelect-select': {
-                  padding: '10px',
-                },
-              }}
-            >
-              <MenuItem value={7}>Last Week</MenuItem>
-              <MenuItem value={14}>Last Two Weeks</MenuItem>
-              <MenuItem value={30}>Last Month</MenuItem>
-            </Select>
-          </FormControl>
-        </Box>
-      </Box>
+              {pastParameter.toUpperCase()} PREDICTIONS FOR{' '}
+              {pastPredictionPeriod === 7
+                ? 'LAST WEEK'
+                : pastPredictionPeriod === 14
+                ? 'LAST TWO WEEKS'
+                : 'LAST MONTH'}
+            </Typography>
 
-      <LineChart
-        xAxis={[{ scaleType: 'point', data: pastDates }]}
-        series={[
-          { color: '#A294F9', data: pastPredictions },
-          { color: '#EB5B00', data: realData },
-        ]}
-        width={450}
-        height={300}
-      />
-       <Box display="flex" justifyContent="center" gap={2} mt={1}>
-    <Box display="flex" alignItems="center">
-      <Box sx={{ width: 12, height: 12, backgroundColor: '#A294F9', borderRadius: '50%', mr: 1 }} />
-      <Typography variant="body2">Predicted Data</Typography>
-    </Box>
-    <Box display="flex" alignItems="center">
-      <Box sx={{ width: 12, height: 12, backgroundColor: '#EB5B00', borderRadius: '50%', mr: 1 }} />
-      <Typography variant="body2">Actual Data</Typography>
-    </Box>
-  </Box>
-    </Card>
+            <Box
+              display="flex"
+              alignItems="center"
+              justifyContent="flex-end"
+              width="100%"
+            >
+              <Box display="flex" gap={1}>
+                <FormControl sx={{m: 1, minWidth: 60}} size="small">
+                  <Select
+                    name="turbidity"
+                    id="demo-select-small"
+                    value={String(pastParameter)}
+                    onChange={handlePastParameterChange}
+                    sx={{
+                      width: '110px',
+                      height: '30px',
+                      fontSize: '12px',
+                      borderRadius: 3,
+                      '& .MuiSelect-select': {
+                        padding: '10px'
+                      }
+                    }}
+                  >
+                    <MenuItem value={'ph'}>Ph</MenuItem>
+                    <MenuItem value={'turbidity'}>Turbidity</MenuItem>
+                    <MenuItem value={'conductivity'}>Conductivity</MenuItem>
+                  </Select>
+                </FormControl>
+
+                <FormControl sx={{m: 1, minWidth: 130}} size="small">
+                  <Select
+                    name="week"
+                    id="demo-select-small"
+                    value={String(pastPredictionPeriod)}
+                    onChange={handlePastPeriodChange}
+                    sx={{
+                      width: '110px',
+                      height: '30px',
+                      fontSize: '12px',
+                      borderRadius: 3,
+                      '& .MuiSelect-select': {
+                        padding: '10px'
+                      }
+                    }}
+                  >
+                    <MenuItem value={7}>Last Week</MenuItem>
+                    <MenuItem value={14}>Last Two Weeks</MenuItem>
+                    <MenuItem value={30}>Last Month</MenuItem>
+                  </Select>
+                </FormControl>
+              </Box>
+            </Box>
+          </Box>
+
+          <LineChart
+            xAxis={[{scaleType: 'point', data: pastDates}]}
+            series={[
+              {color: '#A294F9', data: pastPredictions},
+              {color: '#EB5B00', data: realData}
+            ]}
+            width={450}
+            height={300}
+          />
+          <Box display="flex" justifyContent="center" gap={2} mt={1}>
+            <Box display="flex" alignItems="center">
+              <Box
+                sx={{
+                  width: 12,
+                  height: 12,
+                  backgroundColor: '#A294F9',
+                  borderRadius: '50%',
+                  mr: 1
+                }}
+              />
+              <Typography variant="body2">Predicted Data</Typography>
+            </Box>
+            <Box display="flex" alignItems="center">
+              <Box
+                sx={{
+                  width: 12,
+                  height: 12,
+                  backgroundColor: '#EB5B00',
+                  borderRadius: '50%',
+                  mr: 1
+                }}
+              />
+              <Typography variant="body2">Actual Data</Typography>
+            </Box>
+          </Box>
+        </Card>
         <Card className="cardContainer">
+        <Typography
+    sx={{
+      textAlign: 'center',
+      mb: 2,
+      textTransform: 'uppercase',
+      color: '#102D4D',
+      fontWeight: 'bold'
+    }}
+  >
+    {futureParameter.toUpperCase()} Predictions with Weather Data
+  </Typography>
           <Box
             style={{
               display: 'flex',
@@ -376,36 +469,43 @@ const conductivity  =[58.6, 58.6, 58.3, 56.1, 54.3, 54.3, 54.3, 54.3, 56.1, 54.3
           </Box>
           <Table className="table">
             <TableHead>
-              <TableRow>
-                {[
-                  'Date',
-                  'Parameter',
-                  'Predicted Value',
-                  'Rainfall (mm)',
-                  'Humidity (g/m³)',
-                  'Temperature (°C)',
-                  'Threshold Value',
-                  'Status'
-                ].map((header) => (
-                  <TableCell
-                    key={header}
-                    className="table-header"
-                    sx={header === 'Date' ? {width: '110px'} : {}}
-                  >
-                    {header}
-                  </TableCell>
-                ))}
-              </TableRow>
+            <TableRow>
+  {[
+    'Date',
+    'Parameter',
+    'Predicted Value',
+    'Rainfall (mm)',
+    'Humidity (g/m³)',
+    'Temperature (°C)',
+    'Threshold Value',
+    'Status'
+  ].map((header) => (
+    <TableCell
+      key={header}
+      className="table-header"
+      sx={{
+        backgroundColor: '#f0f0f0',
+        fontWeight: 'bold',
+        color: '#102D4D',
+        padding: '10px',
+        ...(header === 'Date' && { width: '110px' })
+      }}
+    >
+      {header}
+    </TableCell>
+  ))}
+</TableRow>
+
             </TableHead>
             <TableBody>
               {rows.map((row: any, index) => {
                 const predictedValue = futureParameter.includes('ph')
                   ? parseFloat(row.avgPh.toFixed(3))
                   : futureParameter.includes('Conductivity')
-                    ? parseFloat(row.avgConductivity.toFixed(3))
-                    : futureParameter.includes('Turbidity')
-                      ? parseFloat(row.avgTurbidity.toFixed(3))
-                      : 0;
+                  ? parseFloat(row.avgConductivity.toFixed(3))
+                  : futureParameter.includes('Turbidity')
+                  ? parseFloat(row.avgTurbidity.toFixed(3))
+                  : 0;
                 const status =
                   predictedValue > row.threshold ? 'Exceeds' : 'Good';
                 const isActive = status === 'Good';
@@ -442,7 +542,9 @@ const conductivity  =[58.6, 58.6, 58.3, 56.1, 54.3, 54.3, 54.3, 54.3, 56.1, 54.3
                           width: '80px',
                           backgroundColor: isActive ? '#a8f1d4' : '#fdd5d5',
                           color: isActive ? '#008000' : '#ff0000',
-                          border: `1px solid ${isActive ? '#008000' : '#ff0000'}`,
+                          border: `1px solid ${
+                            isActive ? '#008000' : '#ff0000'
+                          }`,
                           borderRadius: '5px',
                           textAlign: 'center',
                           fontWeight: 'bold'
