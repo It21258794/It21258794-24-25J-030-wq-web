a5889f23652237bc10dd40e807e2e618cace750f
hansani k.j.m. <hansanimu00@gmail.com>
2025-05-15 20:52:04 +0530

    add console log to debug lastUsage API response

--- wq-web/src/pages/Chemical Consumption/Chemical_Consumption.tsx
+++ wq-web/src/pages/Chemical Consumption/Chemical_Consumption.tsx
@@ -63,6 +63,11 @@ const Chemical_Consumption = () => {
     }).catch((err) => console.log(err));
   }, []);
 
+  useEffect(() => {
+  console.log("Last Usage Data:", lastUsage);
+}, [lastUsage]);
+
+
   const handleDateChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setSelectedDate(event.target.value);
   };
