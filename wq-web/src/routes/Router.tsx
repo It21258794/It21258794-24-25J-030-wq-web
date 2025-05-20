0ba91bc0d12dcd440b6e56eb42722e0fa6b301e4
IT21174308 <it21174308@my.sliit.lk>
2025-05-21 00:27:38 +0530

    fix(routes):change routes

--- wq-web/src/routes/Router.tsx
+++ wq-web/src/routes/Router.tsx
@@ -16,9 +16,7 @@ import CompareSensors from '../pages/CompareSensors/CompareSensors';
 import App from '../App';
 import UserManagement from '../pages/UserManagement/UserManagement';
 import ChangePassword from '../pages/Common/ChangePassword';
-import AddTest from "../pages/Flow Customization/addTest";
-import AddStep from "../pages/Flow Customization/addStep";
-import AddChemical from "../pages/Flow Customization/addChemical";
+
 import StepView from "../pages/Flow Customization/stepView";
 import Prediction from "../pages/Flow Customization/prediction";
 
@@ -57,9 +55,6 @@ const router = createBrowserRouter([
           },
           {path: 'chemicalConsumption', element: <Chemical_Consumption />},
           {path: 'flowCustomization', element: <Flow_Customization />},
-                  { path: "flow/add-test", element: <AddTest /> },
-                    { path: "flow/add-step", element: <AddStep /> },
-                    { path: "flow/add-chemical", element: <AddChemical /> },
                     { path: "flow/step/:stepId", element: <StepView /> },
                     { path: "flow/prediction", element: <Prediction /> },
           {path: 'reports', element: <Reports />},
