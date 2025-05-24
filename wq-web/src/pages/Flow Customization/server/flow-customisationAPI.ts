9e707c84b5897ce523c9e679b9b0648bf563942e
It21258794 <it21258794@my.sliit.lk>
2025-05-24 15:45:36 +0530

    api change in flow customization

--- wq-web/src/pages/Flow Customization/server/flow-customisationAPI.ts
+++ wq-web/src/pages/Flow Customization/server/flow-customisationAPI.ts
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 // Base API URL
-const BASE_URL = "http://localhost:8080/api";
+const BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 // Function to add a step
 export const createStep = async (
